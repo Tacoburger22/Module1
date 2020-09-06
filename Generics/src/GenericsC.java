e0fdd8d02e5d904dd746997ae26caa9f63def156
isaacweiss <weissic@att.net>
2020-09-05 19:46:19 -0500

    Changed author

--- Generics/src/GenericsC.java
+++ Generics/src/GenericsC.java
@@ -19,7 +19,7 @@ import java.util.List;
  // to eliminate all unchecked warnings. That is,
  // make this code type safe.
  //
- ///////////////////////////////////////////////
+ ////////////////////////////////////////////////
 
 public class GenericsC<T>{
 
